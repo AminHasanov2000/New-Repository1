debd77ad684a028d29a8c9884bcd03bca893bf80
AminHasanov2000 <mastchoh2000@gmail.com>
2023-10-16 17:43:52 -0400

    Added new bug

--- src/FirstCommit.java
+++ src/FirstCommit.java
@@ -2,7 +2,8 @@ public class FirstCommit {
     public static void main(String[] args) {
         System.out.println("First commit");
         System.out.println("Hello there");
-        System.out.println("Is there any problem");
+        System.out.println("Hello mucho");
+
 
     }
 }
