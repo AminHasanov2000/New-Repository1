565cab7daa9881f5b01e064199e4f146d6d67081
AminHasanov2000 <147754079+AminHasanov2000@users.noreply.github.com>
2023-10-16 17:34:39 -0400

    Update FirstCommit.java

--- src/FirstCommit.java
+++ src/FirstCommit.java
@@ -2,6 +2,7 @@ public class FirstCommit {
     public static void main(String[] args) {
         System.out.println("First commit");
         System.out.println("Hello there");
+        // there is some bug in log in 
 
     }
 }
